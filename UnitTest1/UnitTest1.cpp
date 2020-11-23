5504e3ef7a6f222fbb2a28e213f201e3ff74d801
Marharyta-Tkachenko <margoshyn23@gmail.com>
2020-11-23 02:34:04 +0200

    first master commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,25 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Project7.3.2.1/Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int rowCount = 2, colCount = 2;
+			int** a = new int* [rowCount];
+			for (int i = 0; i < rowCount; i++)
+				a[i] = new int[colCount];
+			a[0][0] = 3, a[0][1] = -2, a[1][0] = 12, a[1][1] = -5;
+			int suma = Sum(a, rowCount, colCount, 0, 0, 0);
+			Assert::AreEqual(suma, 8);
+			delete[] a;
+		}
+	};
+}
